9811ad56014a41fe084f6b17bd4bf7365d2a5f7e
McGlasson <DM145057@davenportschools.org>
2019-04-18 10:26:01 -0500

    update

--- TextBasedGame/IntroToGame.java
+++ TextBasedGame/IntroToGame.java
@@ -7,26 +7,13 @@
  */
 public class IntroToGame
 {
-    private int x;
+    private int userName;
 
     /**
      * Constructor for objects of class IntroToGame
      */
     public IntroToGame()
     {
-
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-
-        return x + y;
+        System.out.print("Please enter your name >>> " + userName); 
     }
 }
