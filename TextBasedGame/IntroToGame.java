f07a2ebdc29e887d97397229160c5c04cdcfea94
McGlasson <DM145057@davenportschools.org>
2019-04-18 10:19:58 -0500

    update

---
+++ TextBasedGame/IntroToGame.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class IntroToGame here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class IntroToGame
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class IntroToGame
+     */
+    public IntroToGame()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
